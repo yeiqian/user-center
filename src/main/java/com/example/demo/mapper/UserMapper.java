f4ace25cf860b2fcaf713694370a7701ba8d2395
yeqian <780005915@qq.com>
2024-05-01 22:54:00 +0800

    第一次上传

---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo.mapper;
+
+import com.example.demo.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author admin
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2024-04-29 15:53:55
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
